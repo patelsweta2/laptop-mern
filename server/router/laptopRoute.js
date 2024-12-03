0de81a852dd4692cce8f86852369775a5d692697
patelsweta2 <sweta0442@gmail.com>
2024-12-03 15:07:04 +0530

    update route with middleware

--- server/router/laptopRoute.js
+++ server/router/laptopRoute.js
@@ -6,16 +6,21 @@ import {
   updateLaptop,
   deleteLaptop,
 } from "../controller/laptopController.js";
+import { Authenticate } from "../middleware/authMiddleware.js";
 import { Authorize } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route("/laptop").post(Authorize("admin"), createLaptop);
-router.route("/laptops").get(Authorize("admin"), getAllLaptops);
+router.route("/laptop").post(Authenticate, Authorize("admin"), createLaptop);
+router.route("/laptops").get(Authenticate, Authorize("admin"), getAllLaptops);
 router
   .route("/laptop/:serialNumber")
-  .get(Authorize("admin"), getLaptopBySerialNumber);
-router.route("/laptop/:serialNumber").put(Authorize("admin"), updateLaptop);
-router.route("/laptop/:serialNumber").delete(Authorize("admin"), deleteLaptop);
+  .get(Authenticate, Authorize("admin"), getLaptopBySerialNumber);
+router
+  .route("/laptop/:serialNumber")
+  .put(Authenticate, Authorize("admin"), updateLaptop);
+router
+  .route("/laptop/:serialNumber")
+  .delete(Authenticate, Authorize("admin"), deleteLaptop);
 
 export default router;
