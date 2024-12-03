8278f1f165f618a4202b386f4e4384ca5a8edda5
patelsweta2 <sweta0442@gmail.com>
2024-12-03 22:11:52 +0530

    routing setup is done

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,51 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { lazy, StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import {
+  createBrowserRouter,
+  Route,
+  RouterProvider,
+  createRoutesFromElements,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/configureStore.js";
+const App = lazy(() => import("./App.jsx"));
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-createRoot(document.getElementById('root')).render(
+const LandingPage = lazy(() => import("./pages/LandingPage.jsx"));
+const CustomSuspense = ({ children }) => {
+  return <Suspense fallback={<h3>Loading...</h3>}>{children}</Suspense>;
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+      path="/"
+      element={
+        <CustomSuspense>
+          <App />
+        </CustomSuspense>
+      }
+    >
+      <Route
+        index={true}
+        path="/"
+        element={
+          <CustomSuspense>
+            <LandingPage />
+          </CustomSuspense>
+        }
+      />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <ToastContainer autoClose={1300} />
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
